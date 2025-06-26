b17ab24f4947a00f74e3085d5c3b387e7d24cf97
LeonidGlu <ale.glushckow@yandex.ru>
2025-06-26 13:05:22 +0700

    move registerFormData to folder

--- src/features/types/forms.ts
+++ src/features/types/forms.ts
@@ -2,3 +2,10 @@ export interface LoginFormData {
     email: string
     password: string
 }
+
+export interface RegisterFormData{
+    email:string;
+    username:string;
+    password:string;
+    confirmPassword:string;
+} 
