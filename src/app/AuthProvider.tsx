18cdd67bbf5aaebd2069b106829fb32f30b3b0a2
Saveliy Zhadan <savely.jadan@gmail.com>
2025-06-30 23:45:00 +0700

    feat: add navigation panel

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -1,27 +1,9 @@
-import { createContext, useCallback, useContext } from 'react'
-import type { UserData } from '../types/user'
+import { useCallback } from 'react'
 import { getCookie } from '../utils/cookies'
 import { fetchAuthMe } from '../api/fetchAuth'
 import { useFetch } from '../hooks/useFetch'
-
-interface AuthContextType {
-    user: UserData | null
-    setUser: (user: UserData | null) => void
-    isLoading: boolean
-    error: unknown
-}
-
-const AuthContext = createContext<AuthContextType | undefined>(undefined)
-
-export const useAuth = () => {
-    const context = useContext(AuthContext)
-
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider ')
-    }
-
-    return context
-}
+import type { UserData } from '../types/user'
+import { AuthContext } from '../contexts/AuthContext'
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
     children,
--- src/app/PrivateRouter.tsx
+++ src/app/PrivateRouter.tsx
@@ -1,12 +1,12 @@
 import { Outlet, Navigate } from 'react-router'
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../hooks/useAuth'
 
 export const PrivateRouter = () => {
     const { user } = useAuth()
 
-    if (!user) {
-        return <Navigate to={'auth'} />
-    }
+    // if (!user) {
+    //     return <Navigate to={'login'} />
+    // }
 
     return <Outlet />
 }
