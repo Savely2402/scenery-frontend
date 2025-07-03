cd5ba8fc1efc3a35cf305cabcb5abbddfc543321
Saveliy Zhadan <savely.jadan@gmail.com>
2025-07-03 15:45:05 +0700

    fix: quick fix

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,17 +1,9 @@
 import { AddPost } from '../../../features/AddPost'
-import { useFetch } from '../../../hooks/useFetch'
-import { fetchAllPosts } from '../../../shared/api'
-import { PostItem } from '../../../widgets/PostItem'
 
 export const Main: React.FC = () => {
-    const { data, error, isLoading } = useFetch(fetchAllPosts)
-
     return (
         <>
             <AddPost />
-            {data?.map((post) => (
-                <PostItem post={post} />
-            ))}
         </>
     )
 }
